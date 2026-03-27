2e55d60a4458af4426d2bd0444d7583747f6439f
Jose Rodrigo Sanchez <jose.sanchez@kuantik.mx>
2026-03-27 10:16:17 -0600

    SpiralMatrix finished

--- SpiralMatrix/SpiralMatrix.java
+++ SpiralMatrix/SpiralMatrix.java
@@ -5,31 +5,79 @@ import java.util.List;
 
 public class SpiralMatrix {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    SpiralMatrix sm = new SpiralMatrix();
+    for(Integer i : sm.spiralOrder(new int[][] {
+      {1, 2, 3},
+      {4, 5, 6},
+      {7, 8, 9}
+    })) {
+      System.out.println(i);
+    }
+
   }
 
   public List<Integer> spiralOrder(int[][] matrix) {
 
-    Integer origin = 0;
-    Integer destination = matrix[0].length;
+    Integer horizontalStart = 0;
+    Integer horizontalEnd = matrix[0].length - 1;
+
+    Integer verticalStart = 0;
+    Integer verticalEnd = matrix.length - 1;
 
     List<Integer> result = new ArrayList<>();
 
-    Integer totalSize = destination * matrix.length;
+    Integer totalSize = (horizontalEnd + 1) * (verticalEnd + 1);
+    Integer count = 0;
 
-    for(int s=0; s < totalSize; s++) {
+    do {
+      for (int i = horizontalStart; i <= horizontalEnd; i++) {
+        result.add(matrix[horizontalStart][i]);
+        count++;
 
-      for(int i=0; i < origin; i++) {
-        for(int j=0; j < destination; j++) {
-          result.add(matrix[i][j]);
-        }            
+        if (validateGoal(totalSize, count)) {
+          return result;
+        }
+
       }
 
-    }
+      verticalStart++;
+      for (int i = verticalStart; i <= verticalEnd; i++) {
+        result.add(matrix[i][horizontalEnd]);
+        count++;
 
-    
+        if (validateGoal(totalSize, count)) {
+          return result;
+        }
 
+      }
 
-    return null;
+      horizontalEnd--;
+      for (int i = horizontalEnd; i >= horizontalStart; i--) {
+        result.add(matrix[verticalEnd][i]);
+        count++;
+        if (validateGoal(totalSize, count)) {
+          return result;
+        }
+      }
+
+      verticalEnd--;
+      for (int i = verticalEnd; i >= verticalStart; i--) {
+        result.add(matrix[i][horizontalStart]);
+        count++;
+        if (validateGoal(totalSize, count)) {
+          return result;
+        }
+      }
+
+      horizontalStart++;
+
+    } while (count <= totalSize);
+
+    return result;
+  }
+
+  public boolean validateGoal(int size, int count) {
+    return size == count;
   }
 }
