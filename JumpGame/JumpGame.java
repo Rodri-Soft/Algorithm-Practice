7e763c0650754e1ed18a6589017747e1b28c2bbf
Jose Rodrigo Sanchez <jose.sanchez@kuantik.mx>
2026-03-30 16:17:42 -0600

    JumpGame

---
+++ JumpGame/JumpGame.java
@@ -0,0 +1,42 @@
+package JumpGame;
+
+// You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+// Return true if you can reach the last index, or false otherwise.
+
+// Example 1:
+
+// Input: nums = [2,3,1,1,4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Example 2:
+
+// Input: nums = [3,2,1,0,4]
+// Output: false
+// Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+public class JumpGame {
+  public static void main(String[] args) {
+    // int[] nums = {2,3,1,1,4};
+    int[] nums = { 3, 2, 1, 0, 4 };
+    // int[] nums = { 2,0,0};
+    JumpGame jg = new JumpGame();
+    System.out.println(jg.canJump(nums));
+  }
+
+  public boolean canJump(int[] nums) {
+    int maxReachable = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      // Si el índice actual ya no es alcanzable, atascado
+      if (i > maxReachable)
+        return false;
+
+      // Actualiza el máximo índice al que puedes llegar
+      maxReachable = Math.max(maxReachable, i + nums[i]);
+    }
+
+    return true;
+  }
+
+}
